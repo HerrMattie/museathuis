f28d1c1e3d2b38e7cbfb34bb77c67a5c61e5df47
HerrMattie <mvherregodts@gmail.com>
2025-12-01 20:49:13 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  // leeg of jouw eigen settings
 };
 
 export default nextConfig;
