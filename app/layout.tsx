2e8ed5d472e3998df5a96c983ea33650086e17f0
HerrMattie <mvherregodts@gmail.com>
2025-12-09 09:27:40 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { createClient } from '@/lib/supabaseServer';
+import { cookies } from 'next/headers';
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
 
-// Configureer Google Fonts
 const inter = Inter({ 
   subsets: ["latin"], 
   variable: '--font-inter',
@@ -20,11 +21,17 @@ export const metadata = {
   description: "Dagelijkse kunstbeleving van wereldklasse.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // Check hier alvast of de gebruiker is ingelogd
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: { user } } = await supabase.auth.getUser();
+
   return (
     <html lang="nl" className={`${inter.variable} ${playfair.variable}`}>
       <body className="antialiased pt-20 selection:bg-museum-gold selection:text-black bg-midnight-950">
-        <NavBar />
+        {/* We geven de 'user' mee aan de NavBar */}
+        <NavBar user={user} />
         {children}
       </body>
     </html>
