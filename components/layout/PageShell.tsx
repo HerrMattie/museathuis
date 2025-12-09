ee22d00d94e44b44d22069c3ae8efb6e8fc9f76b
HerrMattie <mvherregodts@gmail.com>
2025-12-09 08:19:25 +0100

    Update PageShell.tsx

--- components/layout/PageShell.tsx
+++ components/layout/PageShell.tsx
@@ -1,17 +1,24 @@
-"use client";
-
 import { ReactNode } from "react";
-import { NavBar } from "./NavBar";
-import { Footer } from "./Footer";
+// DE FIX: Accolades weggehaald omdat NavBar een 'export default' is
+import NavBar from "./NavBar"; 
+// Footer bestaat misschien nog niet, als dat zo is, haal die regel dan ook weg of comment hem uit
+// import { Footer } from "./Footer"; 
 
 export function PageShell({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-midnight-950 text-white">
+      {/* LET OP: Omdat we NavBar nu ook in app/layout.tsx hebben, 
+         kan hij hier dubbel verschijnen. 
+         Voor nu fixen we alleen de error, maar waarschijnlijk 
+         kun je <NavBar /> hieronder verwijderen als je layout.tsx gebruikt.
+      */}
       <NavBar />
-      <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-6 sm:px-6 lg:px-8">
+      
+      <main className="flex-1">
         {children}
       </main>
-      <Footer />
+      
+      {/* <Footer /> */}
     </div>
   );
 }
