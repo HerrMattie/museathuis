3cfbee4e1fa2db0a3efc65849d9542bce262864c
HerrMattie <mvherregodts@gmail.com>
2025-12-14 19:07:31 +0100

    Update Skeleton.tsx

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -1,11 +1,5 @@
 import { cn } from "@/lib/utils";
 
-// Een simpele helper om klassen samen te voegen (als je die nog niet had)
-// Als je geen @/lib/utils hebt, kun je 'cn' weglaten en gewoon de string gebruiken.
-function cn(...classes: (string | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Skeleton({ className, ...props }: SkeletonProps) {
