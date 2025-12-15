fd6278fae9baa62ea99ccce91d7059e5bac8a46e
HerrMattie <mvherregodts@gmail.com>
2025-12-15 13:02:34 +0100

    Update route.ts

--- app/api/import/wikidata/route.ts
+++ app/api/import/wikidata/route.ts
@@ -19,7 +19,7 @@ async function fetchWikidataItems(typeId: string, offset: number) {
         ?item wdt:P31 wd:${typeId}; 
               wdt:P18 ?image;              
               wikibase:sitelinks ?sitelinks. 
-        FILTER(?sitelinks > 5) 
+        FILTER(?sitelinks > 20) 
         OPTIONAL { ?item wdt:P571 ?year. }
         OPTIONAL { ?item wdt:P170 ?artist. }
         SERVICE wikibase:label { bd:serviceParam wikibase:language "nl,en". }
