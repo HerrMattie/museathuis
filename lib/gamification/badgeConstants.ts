94a169d0df088a7871877bb40c55a90f7ccb9d7d
HerrMattie <mvherregodts@gmail.com>
2025-12-23 19:06:46 +0100

    Create badgeConstants.ts

---
+++ lib/gamification/badgeConstants.ts
@@ -0,0 +1,28 @@
+// lib/gamification/badgeConstants.ts
+
+export const BADGE_IDS = {
+  // --- TIJD & DATUM ---
+  NACHTWACHT: '482d3de3-e1f5-4a0a-ae4d-2976dd2a4b9f',      // 00:00 - 04:00
+  VROEGE_VOGEL: '1d7454af-4c38-4738-a67c-8706914a9d8c',    // 05:00 - 07:00
+  LUNCHPAUZE: '64d98c0c-00e7-43ed-83d7-4fd7ed58c3f9',      // 12:00 - 13:00
+  SLAAPKOP: 'ffacf5fe-a2d8-4341-af20-9e7ec0931c71',        // Eerste bezoek > 14:00
+  VRIJMIBO: 'e3138d55-af34-4f95-ac2c-faf772d340de',        // Vrijdag > 17:00
+  OP_DE_VALREEP: 'f7f89b97-546d-4c14-93f6-0d637cf95952',   // 23:50 - 23:59
+
+  // --- SPECIALE DAGEN ---
+  KERST_2025: '056f217f-a3f6-440b-8edc-2fdfe51b62e8',      // 25-26 Dec
+  OLIEBOL: '31ab2192-61c6-4fef-b80b-ee6f3747c231',         // 31 Dec - 1 Jan
+  VALENTIJN: '8da903f2-3af6-47fa-a0c2-dc4fd3040349',       // 14 Feb
+  KONINGSDAG: 'f21272fc-0f04-4024-9ec1-94392601066f',      // 27 Apr
+  GRIEZELIG: '925f79d4-8918-4d48-baff-1afb94cfb70f',       // 31 Okt (Halloween)
+  BLAUWE_MAANDAG: '36265e41-3062-4fbc-9180-b64312a5fcbc',  // 3e maandag Jan
+
+  // --- STREAKS ---
+  DE_KOP_IS_ERAF: '61a92d96-932b-4183-988c-d64052b5abfa',  // 3 dagen
+  WEEK_WINNAAR: 'c80ebaa6-f20d-47c1-a9c1-aae016c89356',    // 7 dagen
+  TWEE_WEKEN_TROUW: '07191855-60d0-4cc6-9319-9a7fed5b4587',// 14 dagen
+  MAAND_MEESTER: '27d21841-1279-43c8-a299-17eb968e5d7c',   // 30 dagen
+  SEIZOENSKAART: '07ff5e03-3ac8-43e7-afcc-e4f8f7758068',   // 90 dagen
+  DE_100_CLUB: 'd3fd1a9c-2f06-42f5-bc9e-df6971a7d3b8',     // 100 dagen
+  JAARRING: 'fb949c6a-d783-423a-8886-5be25b41d760',        // 365 dagen
+};
